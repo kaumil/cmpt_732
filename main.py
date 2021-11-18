2f930b43f9ddc4508fba904d34daeda73be67e24
Gurashish <gurashish62@gmail.com>
2021-11-18 14:17:43 -0800

    Scraping all the data along with all the details

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import configparser
 import os
 import json
-import gzip
+import time
+import shutil
 from pathlib import Path
 import uuid
 
@@ -9,26 +10,31 @@ from api.scraping.scrapers import CADORSPageScrapper, CADORSQueryScrapper
 
 
 def main(scraping_config):
-    print("Starting to scrape the occurance links of all CADORS incidents")
+    # print("Starting to scrape the occurance links of all CADORS incidents")
 
-    # getting occurances
-    query_scrapper = CADORSQueryScrapper(scraping_config)
-    query_scrapper.scrape_occurances()
+    # # getting occurances
+    # query_scrapper = CADORSQueryScrapper(scraping_config)
+    # query_scrapper.scrape_occurances()
 
-    print("Completed scrapping occurance urls.")
+    # print("Completed scrapping occurance urls.")
 
-    print("Starting to scrape page data.")
+    # print("Starting to scrape page data.")
 
     # @gurashish start from over here....
 
     Path(scraping_config["page_data_output_folder"]).mkdir(parents=True, exist_ok=True)
+    Path(scraping_config["scraped_occurances_folder"]).mkdir(
+        parents=True, exist_ok=True
+    )
     cnt = 0
 
     for file in os.listdir(scraping_config["occurances_output_folder"]):
         # for file in fnames:
         file_data = []
+        src = os.path.join(scraping_config["occurances_output_folder"], str(file))
+        dest = os.path.join(scraping_config["scraped_occurances_folder"], str(file))
         with open(
-            os.path.join(scraping_config["occurances_output_folder"], str(file)),
+            src,
             "r",
         ) as f:
             occurances = json.load(f)  # new logic
@@ -41,6 +47,10 @@ def main(scraping_config):
                 # print("\nGURdata\n", file_data)
                 cnt += 1
                 print(cnt)
+
+        print("Moving file")
+        shutil.move(src=src, dst=dest)
+        time.sleep(10)
 
         json_str = json.dumps(file_data)
         # json_bytes = json_str.encode("utf-8")
