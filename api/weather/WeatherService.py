3c04e69bf8b68779e3f2fec8649f8db83a2433e7
Gabriel <52950086+Gabriel737@users.noreply.github.com>
2021-11-18 23:26:50 -0800

    Updated Documentation

--- api/weather/WeatherService.py
+++ api/weather/WeatherService.py
@@ -4,11 +4,15 @@ from geopy.geocoders import Nominatim
 from datetime import datetime
 from meteostat import Point, Daily
 from WeatherServiceExceptions import WeatherServiceFailedToLocateException
+from WeatherServiceExceptions import WeatherServiceFailedToRetrieveException
 
 APP_DESCRIPTION = 'CADORS: Civil Aviation Safety Research - Simon Fraser University, BC, Canada'
 
 class WeatherService:
     def __init__(self):
+        """[Instantiates a WeatherService object]
+        """        
+        
         """ Uses OpenMaps Nominatim API
             Limitation: 60 calls per minute
         """    
@@ -25,6 +29,32 @@ class WeatherService:
         self.province_pattern = re.compile(r'([A-Z]{2})')
 
     def get_weather(self, date, aerodrome_id, occurence_location, province, country):
+        """[Returns a weather object relating to the date and location of the occurence]
+
+        Args:
+            date ([string]): [date in YYYY-MM-DD format]
+            aerodrome_id ([string]): [4-letter ICAO identifier, e.g. CYVR]
+            occurence_location ([string]): [General description of the occurence location]
+            province ([string]): [2-letter province code, e.g. BC]
+            country ([string]): [Full country name, e.g. Canada]
+
+        Raises:
+            WeatherServiceFailedToLocateException: [when the location cannot be matched]
+            WeatherServiceFailedToRetrieveException: [when there is no weather information available near this coordinate]
+
+        Returns:
+            [Weather]: [Dataframe with  the following weather attributes]
+            tavg: average temperature in C
+            tmin: minimum temperature in C
+            tmax: maximum temperature in C
+            prcp: amount of precepitation in mm
+            snow: snow depth in mm
+            wdir: average wind direction in degrees
+            wspd: average wind speed in kph
+            wpgt: peak wind gust speed in kph
+            pres: average sea-level pressure in hPa
+            tsun: total daily sunshine in minutes
+        """        
         
         # Clean the location string, remove province, airport code
         occurence_location = self.parantheses_pattern.sub('', occurence_location)
@@ -48,8 +78,21 @@ class WeatherService:
         point = Point(coordinates.latitude, coordinates.longitude)
         weather = Daily(point, date_time, date_time)
         
-        return occurence_location #weather.fetch()
+        weather_data = weather.fetch()
+        
+        if weather_data.empty:
+            raise WeatherServiceFailedToRetrieveException(date + address)
+        
+        return weather_data
 
     # consider also using mapbox or photon
     def _locate_coordinates(self, location):
+        """[Given a location string, attempt to return an object with the coodinates]
+
+        Args:
+            location ([string]): [a full location string: e.g. Stave Lake, BC, Canada]
+
+        Returns:
+            [Object]: [Object with Latitude and Longitude, or None if it cannot be found]
+        """        
         return self.geolocator.geocode(location)
